e6804cd118f04d93131912d04a1a9d801ac1de88
MayuriKum1999 <88362848+MayuriKum1999@users.noreply.github.com>
2021-09-06 12:40:37 +0530

    Create Registration Page with Stored Procedure

---
+++ 06/PetStore/Web/Registeration.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Registeration.aspx.cs" Inherits="Web.Registeration" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+     <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+    <asp:TextBox for="Label1" ID="TextBox1" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="Email"></asp:Label>
+    <asp:TextBox for="Label2" ID="TextBox2" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label3" runat="server" Text="Zipcode"></asp:Label>
+    <asp:TextBox for="Label3" ID="TextBox3" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label4" runat="server" Text="UserName"></asp:Label>
+     <asp:TextBox for="Label4" ID="TextBox4" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label5" runat="server" Text="Password"></asp:Label>
+    <asp:TextBox for="Label5" ID="TextBox5" runat="server"></asp:TextBox>
+
+    <br />
+     <asp:Button ID="Button1" runat="server" Text="Save" OnClick="Button1_Click" />
+</asp:Content>
---
+++ 06/PetStore/Web/Registeration.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Web
+{
+    public partial class Registeration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string name = TextBox1.Text;
+            string email = TextBox2.Text;
+            string username = TextBox4.Text;
+            string password = TextBox5.Text;
+            string zipcode = TextBox3.Text;
+            string query = "CustomerRegistration";
+            string selectCustId = "Select * from customer";
+            string ConString = ConfigurationManager.ConnectionStrings["PetDbConnectionString"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(ConString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    SqlParameter parameter;
+                    parameter = command.Parameters.Add("@Name", SqlDbType.VarChar);
+                    parameter.Value = name;
+                    parameter = command.Parameters.Add("@Zipcode", SqlDbType.VarChar);
+                    parameter.Value = zipcode;
+                    parameter = command.Parameters.Add("@Email", SqlDbType.VarChar);
+                    parameter.Value = email;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            int custid = 0;
+            using (SqlConnection connection1 = new SqlConnection(ConString))
+            {
+                using (SqlDataAdapter adapter = new SqlDataAdapter(selectCustId, connection1))
+                {
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        if (row["email"].ToString() == email)
+                        {
+                            custid = Convert.ToInt32(row["id"]);
+                        }
+                    }
+                }
+                using (SqlCommand command = new SqlCommand("CustomerLogin", connection1))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    SqlParameter parameter;
+                    parameter = command.Parameters.Add("@CustomerId", SqlDbType.Int);
+                    parameter.Value = custid;
+                    parameter = command.Parameters.Add("@UserName", SqlDbType.VarChar);
+                    parameter.Value = username;
+                    parameter = command.Parameters.Add("@Password", SqlDbType.VarChar);
+                    parameter.Value = password;
+                    connection1.Open();
+                    command.ExecuteNonQuery();
+                    connection1.Close();
+                }
+            }
+        }
+        }
+}
